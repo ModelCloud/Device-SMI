3e8dc9c4e8a7d694cdd68c56a35f8cbdea570c54
CSY-ModelCloud <csy@modelcloud.ai>
2024-11-18 10:24:02 +0800

    update version

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup, find_packages
 
+__version__ = "0.0.1-dev"
+
 setup(
     name="device-smi",
-    version="0.0.0-dev",
-    author= "ModelCloud",
-    author_email= "qubitium@modelcloud.ai",
-    description= "It's designed to retrieve system information about devices.",
+    version=__version__,
+    author="ModelCloud",
+    author_email="qubitium@modelcloud.ai",
+    description="Device-SMI is designed to retrieve system information about devices.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ModelCloud/Device-SMI/",
     packages=find_packages(),
     install_requires=[],
-    platform = ["linux"],
+    platform=["linux"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
