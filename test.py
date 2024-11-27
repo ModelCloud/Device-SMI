4f6344b77d416be7d9599e0f54e8e36e917e379f
CSY-ModelCloud <csy@modelcloud.ai>
2024-11-27 17:49:26 +0800

    revert

--- test.py
+++ test.py
@@ -1,15 +1,7 @@
 from device_smi import Device
 
-smi = Device("cpu")
-info = smi.info()
-print(info.__dict__)
-
-smi = Device("cuda")
-info = smi.info()
-print(info.__dict__)
-
-# for d in ["gpu", "cpu"]:
-#     smi = Device(d)
-#     info = smi.info()
-#     print(info.__dict__)
-#     print(f"{d} used {smi.memory_used() / 1024 / 1024 / 1024:.2f} GB")
+for d in ["gpu", "cpu"]:
+    smi = Device(d)
+    info = smi.info()
+    print(info.__dict__)
+    print(f"{d} used {smi.memory_used() / 1024 / 1024 / 1024:.2f} GB")
