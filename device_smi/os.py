85b495be2a1b7f68d7fb27eda6880a9cf07da6e0
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-02 15:52:00 +0800

    use linux parser for freebsd and solaris

--- device_smi/os.py
+++ device_smi/os.py
@@ -11,20 +11,7 @@ class OSDevice(BaseDevice):
     def __init__(self, cls):
         super().__init__(cls, "os")
 
-        if platform.system().lower() == "freebsd" or platform.system().lower() == "solaris":
-            try:
-                print(_run(["cat", "/etc/os-release"]))
-            except BaseException as e:
-                print(f"eeeeeeeee1 {e}")
-            try:
-                print(_run(["uname", "-s"]))
-                print(_run(["uname", "-r"]))
-                print(_run(["uname", "-v"]))
-            except BaseException as e:
-                print(f"eeeeeeeee2 {e}")
-
-
-        if platform.system().lower() == "linux":
+        if platform.system().lower() == "linux" or platform.system().lower() == "freebsd" or platform.system().lower() == "solaris":
             release_info = self.to_dict(_run(["cat", "/etc/os-release"]).replace("\"", "").lower(), "=")
             cls.name = release_info["name"]
             cls.version = release_info["version_id"]
