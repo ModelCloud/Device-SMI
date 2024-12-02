fc7d1007857885c24f3a6f7607db4861f9ecce88
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-02 13:08:57 +0800

    add type for device

--- device_smi/os.py
+++ device_smi/os.py
@@ -11,6 +11,7 @@ class OSDevice(BaseDevice):
     def __init__(self, cls):
         super().__init__(0)
 
+        cls.type = "os"
         cls.name = platform.system().lower()
         cls.version = platform.version().lower()
         cls.arch = platform.architecture()[0].lower().removesuffix("bit").strip()
