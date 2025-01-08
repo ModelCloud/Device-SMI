ae516f46359c645945da957cdd93bb4959b25f34
CSY <csy@modelcloud.ai>
2025-01-08 02:28:03 +0000

    assert not none

--- tests/gpu.py
+++ tests/gpu.py
@@ -5,11 +5,11 @@ print(dev)
 
 assert dev.type == "gpu", f"wrong type: {dev.type}"
 if dev.pcie:
-    assert dev.pcie.gen
-    assert dev.pcie.speed
-    assert dev.pcie.id
+    assert dev.pcie.gen is not None
+    assert dev.pcie.speed is not None
+    assert dev.pcie.id is not None
 if dev.gpu:
-    assert dev.gpu.driver
-    assert dev.gpu.firmware
+    assert dev.gpu.driver is not None
+    assert dev.gpu.firmware is not None
 assert dev.model
 assert dev.memory_total > 10, f"wrong memory size: {dev.memory_total()}"
