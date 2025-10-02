5b182cd5f2adca735a0161758e172f1b7b3d060f
Qubitium-ModelCloud <qubitium@modelcloud.ai>
2025-10-02 13:53:46 +0800

    fix thread close state (#91)
    * fix thread close state
    
    * patch version

--- format/format.sh
+++ format/format.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")" || exit
 # force ruff/isort to be same version as setup.py
 pip install -U ruff==0.9.6 isort==6.0.0
 
-ruff check ../device_smi ../tests ../setup.py --fix --unsafe-fixes
+ruff check ../device_smi ../tests --fix --unsafe-fixes
 ruff_status=$?
 
 isort -l 119 -e ../
