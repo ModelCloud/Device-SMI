9ed93f9aebb2c7ca804e59c077e9ea355296accc
CSY-ModelCloud <csy@modelcloud.ai>
2025-11-20 11:02:12 +0800

    fix cpu count

--- device_smi/cpu.py
+++ device_smi/cpu.py
@@ -38,6 +38,7 @@ class CPUDevice(BaseDevice):
                 command_result = _run(["powershell", "-NoLogo", "-NoProfile", "-Command", "Get-CimInstance Win32_Processor | Select-Object Manufacturer, Name, NumberOfCores, NumberOfLogicalProcessors"]).strip()
 
                 lines = [line.strip() for line in command_result.splitlines() if line.strip()]
+                cpu_count = command_result.count('\n') - 2
                 data_line = lines[2]
 
                 parts = data_line.split()
